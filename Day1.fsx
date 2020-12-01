571980f1f2bb18928a39678feb917ebc95fe1c61
Kristian Schmidt <kristian@kreutz.us>
2020-12-01 06:10:52 +0100

    Solved day 1

--- Day1.fsx
+++ Day1.fsx
@@ -4,14 +4,29 @@ open System
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 1
+let data = Helpers.Web.getInput 1 |> Array.map int
 
-let ans1 = data
+let (sum1,sum2) =
+    seq {
+        for i in data do
+            for j in data do
+                yield (i,j)
+    } |> Seq.find (fun (i,j) -> i+j=2020)
+
+let ans1 = sum1 * sum2
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let (sum1',sum2',sum3') =
+    seq {
+        for i in data do
+            for j in data do
+                for k in data do
+                    yield (i,j,k)
+    } |> Seq.find (fun (i,j,k) -> i+j+k=2020)
+
+let ans2 = sum1'*sum2'*sum3'
 
 ans2
