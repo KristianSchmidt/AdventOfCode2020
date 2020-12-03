fa14f195aa478d5e831660b4d846214da61c341d
Kristian Schmidt <kristian@kreutz.us>
2020-12-03 06:21:32 +0100

    Day 3 solution

--- Day3.fsx
+++ Day3.fsx
@@ -4,14 +4,34 @@ open System
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 3
+let data =
+    Helpers.Web.getInput 3
+    |> Array.map (fun s -> s.ToCharArray())
 
-let ans1 = data
+let (xLength, yLength) = (data.[0].Length, data.Length)
+
+let solve (xInc, yInc) =
+    let rec loop (x,y) count =
+        if (y >= yLength) then
+            count
+        else
+            let c = data.[y].[x]
+            let nextX = (x + xInc) % xLength
+            match c with
+            | '.' -> loop (nextX, y+yInc) count
+            | '#' -> loop (nextX, y+yInc) (count+1)
+
+    loop (xInc,yInc) 0
+
+let ans1 = solve (3,1)
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let ans2 =
+    [| (1,1); (3,1); (5,1); (7,1); (1,2) |]
+    |> Array.map (solve >> int64)
+    |> Array.reduce (*)
 
 ans2
