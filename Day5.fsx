550f7ee2acd810eafe2708d389312fb3a99d69f5
Kristian Schmidt <kristian@kreutz.us>
2020-12-05 07:32:00 +0100

    Day 5 solution

--- Day5.fsx
+++ Day5.fsx
@@ -4,14 +4,30 @@ open System
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 5
+let toBinary c1 c0 (s : string) =
+    Convert.ToInt32(s.Replace(c1, '1').Replace(c0, '0'), 2)
 
-let ans1 = data
+let data =
+    Helpers.Web.getInput 5
+    |> Array.map (fun s -> toBinary 'B' 'F' (s.Substring(0,7)), toBinary 'R' 'L' (s.Substring(7)))
+
+let seatIds =
+    data
+    |> Array.map (fun (row,col) -> row*8+col)
+    |> Array.sort
+
+let ans1 = Array.max seatIds
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let firstId = Array.head seatIds
+
+let ans2 =
+    seatIds
+    |> Array.mapi (fun i x -> x-(firstId+i), x)
+    |> Array.find (fst >> (<>)0)
+    |> (fun (_,x) -> x-1)
 
 ans2
