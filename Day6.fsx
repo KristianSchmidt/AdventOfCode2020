a68560f510b22dc59f972f882501b00cd0c1e0a4
Kristian Schmidt <kristian@kreutz.us>
2020-12-07 07:57:18 +0100

    Day 6 + 7

--- Day6.fsx
+++ Day6.fsx
@@ -6,12 +6,38 @@ Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 let data = Helpers.Web.getInput 6
 
-let ans1 = data
+let groups =
+    let rec loop p (grps : string list list) currGrp =
+        match p with
+        | x :: [] ->
+            (x :: currGrp) :: grps
+        | x :: xs ->
+            match x with 
+            | "" -> loop xs (currGrp :: grps) []
+            | s -> loop xs grps (s :: currGrp)
 
+    loop (List.ofArray data) [] []
+    |> Array.ofList
+    |> Array.map Array.ofList
+
+let ans1 =
+    groups
+    |> Array.map (Array.collect (fun s -> s.ToCharArray()))
+    |> Array.sumBy (Array.distinct >> Array.length)
+    
 ans1
 
 /// Part 2
 
-let ans2 = data
+let processGroup (grp : string array) =
+    let questions = grp |> Array.collect (fun s -> s.ToCharArray())
+                        |> Array.distinct
+                        |> Array.map string
+
+    let allHasAnswered q = grp |> Array.forall (fun s -> s.Contains(q))
+
+    questions |> Array.filter allHasAnswered |> Array.length
+
+let ans2 = groups |> Array.sumBy processGroup
 
 ans2
--- Day7.fsx
+++ Day7.fsx
@@ -1,17 +1,65 @@
 #load "Helpers.fsx"
 
 open System
+open System.Text.RegularExpressions
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 let data = Helpers.Web.getInput 7
 
-let ans1 = data
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
+
+let parseBagContents =
+    function
+    | "no other" -> None
+    | Regex @"(\d+) (.+)" [ count; bag ] -> Some (int count,bag)
+
+let map =
+    data
+    |> Array.map (fun s -> s.Split([|" bags contain "|], StringSplitOptions.None))
+    |> Array.map (fun [|a1;a2|] -> a1, a2.Replace(" bags", "").Replace(" bag", "").Replace(".", "").Split([|", "|],StringSplitOptions.None))
+    |> Array.map (fun (bag, contents) -> bag, contents |> Array.choose parseBagContents)
+    |> Map.ofArray
+
+let part1graph =
+    map
+    |> Map.toArray
+    |> Array.collect (fun (bag,contents) -> contents |> Array.map (fun c -> (bag,snd c)))
+
+let solve1 (data : (string*string) array) =
+    let rec f data queue res =
+        match queue with
+        | [] -> res
+        | x :: xs ->
+            let bags =
+                data
+                |> Array.filter (snd >> (=)x)
+                |> Array.map fst
+                |> List.ofArray
+
+            let newQueue = List.concat [bags; xs]
+            let newRes = Set.union res (Set.ofList bags)
+            f data newQueue newRes
+
+    f data [ "shiny gold" ] Set.empty
+
+let ans1 = solve1 part1graph |> Set.count
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let solve2 (data : (Map<string,(int*string) array>)) =
+    let rec f start =
+        data
+        |> Map.find start
+        |> Array.sumBy (fun (i,newBag) -> i + i * (f newBag))
+    
+    f "shiny gold"
+
+let ans2 = solve2 map
 
 ans2
