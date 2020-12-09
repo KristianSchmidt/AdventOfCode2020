9bba2076e7f500740de37b353fc65f05b91ecb00
Kristian Schmidt <kristian@kreutz.us>
2020-12-09 07:42:13 +0100

    Solved day 9

--- Day9.fsx
+++ Day9.fsx
@@ -4,14 +4,47 @@ open System
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 9
+let data = Helpers.Web.getInput 9 |> Array.map int64
 
-let ans1 = data
+let isNotInLast25 i target =
+    if (i < 25) then
+        false
+    else
+        seq {
+            for j in (i-25) .. i-2 do
+                for k in (j+1) .. (i-1) do
+                    yield data.[j] + data.[k]
+        }
+        |> Seq.tryFind ((=)target)
+        |> Option.isNone
+
+let ans1 =
+    data
+    |> Seq.mapi (fun i t -> t, isNotInLast25 i t)
+    |> Seq.find snd
+    |> fst
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let maxIdx = (data |> Seq.findIndex ((=)ans1)) - 1
+
+let tryFindContiq startIdx =
+    let rec f i nums acc =
+        let newAcc = acc + data.[i]
+        match newAcc with
+        | _ when newAcc = ans1 -> Some (startIdx, i, nums)
+        | _ when newAcc > ans1 -> None
+        | _ -> f (i+1) (data.[i] :: nums) newAcc
+
+    f startIdx [] 0L
+
+let ans2 =
+    [| 0 .. maxIdx |]
+    |> Seq.choose tryFindContiq
+    |> Seq.exactlyOne
+    |> (fun (_,_,xs) -> List.max xs + List.min xs)
+    
 
 ans2
