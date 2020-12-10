eeac7f8b99d4722e4a1f8012556b537cf23ac5cf
Kristian Schmidt <kristian@kreutz.us>
2020-12-10 22:01:35 +0100

    Day 10 solution

--- Day10.fsx
+++ Day10.fsx
@@ -4,14 +4,45 @@ open System
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 10
+let data = Helpers.Web.getInput 10 |> Array.map int
 
-let ans1 = data
+let maxJolt = Array.max data
+
+let ans1 =
+    Array.concat [| data; [|0; maxJolt+3 |]|]
+    |> Array.sort
+    |> Array.windowed 2
+    |> Array.map (fun [|a1;a2|] -> a2-a1)
+    |> Array.countBy id
+    |> Array.map snd
+    |> Array.reduce (*)
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let vertices =
+    Array.concat [| data; [|0; maxJolt+3 |]|]
+    |> Array.sort
+
+let answers = Array.init vertices.Length (fun _ -> 0L)
+
+let pathsFromIdx i =
+    let paths =
+        [|i+1;i+2;i+3|]
+        |> Array.filter (fun j -> j < vertices.Length && vertices.[j] - vertices.[i] <= 3)
+        |> Array.sumBy (fun j -> answers.[j])
+
+    answers.[i] <- paths
+    paths
+
+// Initial condition
+answers.[answers.Length-1] <- 1L
+
+let ans2 =
+    [| 0 .. (answers.Length-2) |]
+    |> Array.rev
+    |> Array.map pathsFromIdx
+    |> Array.last
 
 ans2
