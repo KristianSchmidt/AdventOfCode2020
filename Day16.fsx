380afeaf7296e8d63c911da5242ae140f6893ee9
Kristian Schmidt <kristian@kreutz.us>
2020-12-16 20:16:31 +0100

    Day 16

--- Day16.fsx
+++ Day16.fsx
@@ -51,8 +51,49 @@ let validNearbyTickets =
     nearbyTickets
     |> Array.filter (ticketIsInvalid >> fst >> not)
 
+let getColumn i =
+    validNearbyTickets |> Array.map (fun a -> a.[i])
+
 let constraints2 = constraintsTxt |> Array.map extractConstraints
 
-let ans2 = data
+let constraintsFulfilled constraints data =
+    constraints
+    |> Array.filter
+        (fun (_,[|(d1,d2);(d3,d4)|]) ->
+            data
+            |> Array.forall (fun i -> d1 <= i && i <= d2 ||
+                                      d3 <= i && i <= d4))
+
+let solve2 constraints =
+    let rec f cons found cols =
+        if (Set.isEmpty cols) then
+            found
+        else
+            let newCol,colNum =
+                cols
+                |> Set.toArray
+                |> Array.map (fun i ->
+                    i,(getColumn i |> constraintsFulfilled cons)
+                    )
+                |> Array.find (snd >> Array.length >> (=)1)
+                |> (fun (i,[|s,_|]) -> s,i)
+
+            let newFound = found |> Map.add newCol colNum
+            let newCols = cols |> Set.remove colNum
+            let newCons = cons |> Array.filter (fst >> (<>)newCol)
+            f newCons newFound newCols
+            
+    f constraints Map.empty (Set.ofArray [|0..(constraints.Length-1)|])
+
+let myTicket =
+    let idx = Array.IndexOf(data, "your ticket:")
+    data.[idx+1] |> split "," |> Array.map int
+ 
+let ans2 =
+    solve2 constraints2
+    |> Map.toArray
+    |> Array.filter (fst >> (fun s -> s.StartsWith("departure")))
+    |> Array.map (snd >> (Array.get myTicket) >> int64)
+    |> Array.reduce (*)
 
 ans2
