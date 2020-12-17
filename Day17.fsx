55ac4e791e8f5ef6433f759c86106133000ba5cb
Kristian Schmidt <kst@siteimprove.com>
2020-12-17 12:49:22 +0100

    Day 17 done

--- Day17.fsx
+++ Day17.fsx
@@ -13,112 +13,73 @@ let data =
             |> Array.mapi (fun x c -> ((x,y),c)))
     |> Array.collect id
 
-let maxX = data |> Array.map (fst >> fst) |> Array.max
-let maxY = data |> Array.map (fst >> snd) |> Array.max
+let dim3combinations = product [-1;0;1] 3 |> List.filter ((<>)[0;0;0]) |> Array.ofList
 
 let adj (x,y,z) =
-    [|
-    (x-1,y+1,z+1); (x,y+1,z+1);(x+1,y+1,z+1)
-    (x-1,y,  z+1); (x,y,  z+1);(x+1,y,  z+1)
-    (x-1,y-1,z+1); (x,y-1,z+1);(x+1,y-1,z+1);
+    dim3combinations
+    |> Array.map (fun [x';y';z'] -> (x+x',y+y',z+z'))
 
-    (x-1,y+1,z);   (x,y+1,z);  (x+1,y+1,z)
-    (x-1,y  ,z);               (x+1,y,  z)
-    (x-1,y-1,z);   (x,y-1,z);  (x+1,y-1,z);
-
-    (x-1,y+1,z-1); (x,y+1,z-1); (x+1,y+1,z-1)
-    (x-1,y,  z-1); (x,y,  z-1); (x+1,y,  z-1)
-    (x-1,y-1,z-1); (x,y-1,z-1); (x+1,y-1,z-1);
-    |]
-    |> Array.filter (fun (x,y,z) -> x >= 0 && x < maxX && y >= 0 && y < maxY)
-
-let allZcoords z =
-    seq {
-        for x in 0 .. maxX do
-            for y in 0 .. maxY do
-                yield (x,y,z)
-    }
-    |> Array.ofSeq
-
-let cycleCoord state (x,y,z) =
+let cycleCoord adj state coord =
     let currState =
         state
-        |> Map.tryFind (x,y,z)
+        |> Map.tryFind coord
         |> Option.defaultValue '.'
-
-    //printfn $"Currstate: {currState}"
-    
+            
     let neighbors =
-        adj (x,y,z)
+        adj coord
         |> Array.map (fun c -> Map.tryFind c state |> Option.defaultValue '.')
-    
-    //printfn "Neigh: %A" neighbors
-    
+        
     let activeNeighbors = neighbors |> Seq.filter ((=)'#') |> Seq.length
-    //printfn "active: %i" activeNeighbors
+    
     match currState with
-    | '#' when activeNeighbors = 2 || activeNeighbors = 3 ->
-        '#'
-    | '.' when activeNeighbors = 3 ->
-        '#'
+    | '#' when activeNeighbors = 2 || activeNeighbors = 3 -> '#'
+    | '.' when activeNeighbors = 3 -> '#'
     | _ -> '.'
 
-let solve (data : ((int*int)*char) array) cycles =
-    let startingMap =
-        data
-        |> Array.map (fun ((x,y),c) -> (x,y,0),c)
-        |> Map.ofArray
-
+let solve startingMap adj cycles =
     let rec f state cyclesLeft =
         if (cyclesLeft = 0) then
-            state
+            state |> Map.filter (fun k v -> v = '#') |> Map.count
         else
-            let zMin,zMax =
-                state
-                |> Map.toSeq
-                |> Seq.map (fun ((_,_,z),_) -> z)
-                |> Seq.distinct
-                |> Seq.toArray
-                |> (fun arr -> (Array.min arr,Array.max arr))
-
             let newCoords =
-                let oldCoords = state |> Map.toArray |> Array.map fst
-                Array.concat
-                    [| allZcoords (zMin-1)
-                       oldCoords
-                       allZcoords (zMax+1)
-                    |]
+                let keyValues = Map.toArray state
+                let oldActive = keyValues |> Array.filter (snd >> (=)'#') |> Array.map fst
+                let newCandidates = oldActive |> Array.collect adj // neighbors of all actives
+                let oldCoords = keyValues |> Array.map fst
+                Array.concat [| oldCoords; newCandidates |]
                 |> Array.distinct
 
             let newState =
                 newCoords
-                |> Array.map (fun c -> c, cycleCoord state c)
+                |> Array.map (fun c -> c, cycleCoord adj state c)
                 |> Map.ofArray
 
             f newState (cyclesLeft-1)
 
     f startingMap cycles
 
-let ans1 =
-    solve data 6
-    |> Map.toArray
-    |> Array.map snd
-    |> Array.filter ((=)'#')
-    |> Array.length
+let startingMap =
+    data
+    |> Array.map (fun ((x,y),c) -> (x,y,0),c)
+    |> Map.ofArray
 
+let ans1 = solve startingMap adj 6
 
-solve data 1
-|> Map.filter (fun (x,y,z) c -> x = 1 && y = 0)
-
-let startState = data |> Array.map (fun ((x,y),c) -> (x,y,0),c) |> Map.ofArray
-
-cycleCoord startState (1,0,-1)
-
-adj (1,0,-1)
 ans1
 
 /// Part 2
 
-let ans2 = data
+let dim4combinations = product [-1;0;1] 4 |> List.filter ((<>)[0;0;0;0]) |> Array.ofList
+
+let adj2 (x,y,z,w) =
+    dim4combinations
+    |> Array.map (fun [x';y';z';w'] -> (x+x',y+y',z+z',w+w'))
+
+let startingMap2 =
+    data
+    |> Array.map (fun ((x,y),c) -> (x,y,0,0),c)
+    |> Map.ofArray
+
+let ans2 = solve startingMap2 adj2 6
 
 ans2
