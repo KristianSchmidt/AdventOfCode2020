c1b60bf62a761559c5e017e831120384d82be773
Kristian Schmidt <kristian@kreutz.us>
2020-12-24 12:41:57 +0100

    Day 23 + 24

--- Day23.fsx
+++ Day23.fsx
@@ -1,17 +1,118 @@
 #load "Helpers.fsx"
 
 open System
+open System.Collections.Generic
+open Helpers
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 23
+let data = "589174263".ToCharArray() |> Array.map (string >> int)
+//let data = "389125467".ToCharArray() |> Array.map (string >> int)
 
-let ans1 = data
+let x = ResizeArray<int>(data)
+let mutable currLabel = x.[0]
+for i in 0..99 do
+    let idx = x.FindIndex(fun i -> i = currLabel)
+    let nextLabel = x.[(idx+4) % data.Length]
+    printfn "Idx: %i" idx
+    printfn "NextLabel: %i" nextLabel
+
+    let nextThree =
+        let idxs = [|idx+1;idx+2;idx+3|]
+                   |> Array.map (fun i -> i % data.Length)
+        let vals = idxs |> Array.map (fun i -> x.[i])
+        idxs |> Array.sortDescending |> Array.iter (fun i -> x.RemoveAt(i))
+        vals
+        //x.GetRange(idx+1,3)
+    //x.RemoveRange(idx+1,3)
+    printf "Pick up: "
+    nextThree |> Seq.iter (printf "%i ")
+    printfn ""
+
+    let rec findInsertIndex labelToFind =
+        //printfn "Trying to find %i" labelToFind
+        let idx = x.FindIndex(fun i -> i = labelToFind)
+        if (idx = -1) then
+            let nextLabel = labelToFind - 1
+            if (nextLabel <= 0) then
+                let max = Seq.max x
+                findInsertIndex max
+            else
+                findInsertIndex nextLabel
+        else
+            idx+1
+
+    let insertIdx = findInsertIndex (currLabel - 1)
+    x.InsertRange(insertIdx,nextThree)
+
+    currLabel <- nextLabel
+
+    x |> Seq.iter (printf "%i ")
+    printfn ""
+    printfn ""
+
+let ans1 =
+    let idx1 = x.FindIndex(fun i -> i = 1)
+    [|1..data.Length-1|]
+    |> Array.map (fun i -> (idx1 + i) % data.Length)
+    |> Array.map (fun i -> x.[i])
+    |> (fun arr -> String.Join("",arr))
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let y =
+    let maxOrigElement = Array.max data
+    let orig = Array.pairwise data
+    let extra = [| (maxOrigElement + 1) .. 1_000_000 |] |> Array.pairwise
+    let binders = [| (data.[data.Length-1], maxOrigElement + 1); (1_000_000,data.[0]) |]
+    Array.concat [|orig;extra;binders|]
+    |> Map.ofArray
+
+let findNextThree (map : Map<int,int>) elem =
+    let fstNeighbor = Map.find elem map
+    let sndNeighbor = Map.find fstNeighbor map
+    let trdNeighbor = Map.find sndNeighbor map
+    [fstNeighbor;sndNeighbor;trdNeighbor]
+
+let solve (map : Map<int,int>) iters firstElem =
+    let maxElem = map |> Map.toSeq |> Seq.map fst |> Seq.max
+
+    let rec getDestCup guess neighbors =
+        if (List.contains guess neighbors) then
+            getDestCup (guess-1) neighbors
+        else if (guess = 0) then
+            getDestCup maxElem neighbors
+        else
+            guess
+
+    let rec doIter (map : Map<int,int>) iterLeft currLabel =
+        if (iterLeft = 0) then
+            map
+        else
+            if (iterLeft % 100_000 = 0) then printfn "Iter: %i" iterLeft
+
+            let neighbors = findNextThree map currLabel
+            let [fstNeighbor;sndNeighbor;trdNeighbor] = neighbors
+            let nextLabel = Map.find trdNeighbor map
+
+            let newMap1 = Map.add currLabel nextLabel map
+            let insertionLabel = getDestCup (currLabel-1) neighbors
+            let insertionLabelNeighbor = Map.find insertionLabel map
+            let newMap2 =
+                Map.add insertionLabel fstNeighbor newMap1
+                |> Map.add trdNeighbor insertionLabelNeighbor
+        
+            doIter newMap2 (iterLeft-1) nextLabel
+
+    doIter map iters firstElem
+
+let finalMap = solve y 10_000_000 data.[0]
+
+let ans2 =
+    let n1 = Map.find 1 finalMap
+    let n2 = Map.find n1 finalMap
+    (int64 n1) * (int64 n2)
 
 ans2
--- Day24.fsx
+++ Day24.fsx
@@ -1,17 +1,174 @@
 #load "Helpers.fsx"
 
 open System
+open System.IO
+open Helpers
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 let data = Helpers.Web.getInput 24
 
-let ans1 = data
+type Direction =
+    | NW | SW | W | E | NE | SE
+
+let parseTile (s : string) =
+    let sr = new StringReader(s)
+    //let sb = StringBuilder()
+    let mutable lst = List.empty
+
+    while (sr.Peek() <> -1) do
+        let char1 = sr.Read()
+        let char2 = sr.Peek()
+        //printfn "%c %c" (char char1) (char char2)
+        //printfn "%i %i" (char1) (char2)
+        //printfn "%A" lst
+        if (char2 <> -1) then
+            let res = 
+                match char char1, char char2 with
+                | 'n','w' -> sr.Read() |> ignore;NW
+                | 's','w' -> sr.Read() |> ignore;SW
+                | 'n','e' -> sr.Read() |> ignore;NE
+                | 's','e' -> sr.Read() |> ignore;SE
+                | 'e',_ -> E
+                | 'w',_ -> W
+            
+            lst <- res :: lst
+        else
+            let res =
+                match char char1 with
+                | 'e' -> E
+                | 'w' -> W
+            lst <- res :: lst
+                
+    List.rev lst
+
+let moveEven =
+    function
+    | NE -> (1,-1)
+    | NW -> (0,-1)
+    | E ->  (1,0)
+    | W ->  (-1,0)
+    | SE -> (1,1)
+    | SW -> (0,1)
+
+let moveOdd =
+    function
+    | NE -> (0,-1)
+    | NW -> (-1,-1)
+    | E ->  (1,0)
+    | W ->  (-1,0)
+    | SE -> (0,1)
+    | SW -> (-1,1)
+
+
+let tileCoord2 (lst : Direction list) =
+    lst
+    |> List.fold
+        (fun (x,y) dir ->
+            let (x',y') =
+                if (y % 2 = 0) then
+                    moveEven dir
+                else
+                    moveOdd dir
+            (x+x',y+y'))
+            (0,0)
+
+let ans1 =
+    data
+    |> Array.map (parseTile >> tileCoord2)
+    |> Array.groupBy id
+    |> Array.map (fun (_,arr) -> Array.length arr)
+    |> Array.filter (fun i -> i % 2 = 1)
+    |> Array.length
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let blackTiles =
+    data
+    |> Array.map (parseTile >> tileCoord2)
+    |> Array.groupBy id
+    |> Array.filter (fun (_,arr) -> Array.length arr % 2 = 1)
+    |> Array.map fst
+
+let neighbors (x,y) =
+    if (y % 2 = 0) then
+        [| (x+1,y-1);(x+0,y-1);(x+1,y+0);(x-1,y+0);(x+1,y+1);(x+0,y+1)|]
+    else
+        [|(x+0,y-1);(x-1,y-1);(x+1,y+0);(x-1,y+0);(x+0,y+1);(x-1,y+1)|]
+
+let solve2 blackTiles iters =
+    let initState = Set.ofArray blackTiles
+
+    let rec f state iters =
+        //printfn "%i" (Set.count state)
+        //printfn "%A" state
+
+        if (iters = 0) then
+            state
+        else
+            let candidates =
+                let n = state |> Set.toArray |> Array.collect neighbors
+                Array.concat [| (Set.toArray state); n|]
+                |> Array.distinct
+
+            let isBlackNextIter c =
+                let isBlack = Set.contains c state
+                let blackNeighbors =
+                    neighbors c
+                    |> Seq.filter (fun c -> Set.contains c state)
+                    |> Seq.length
+                match isBlack, blackNeighbors with
+                | true, 0 -> false
+                | true, x when x > 2 -> false
+                | false, 2 -> true
+                | _,_ -> isBlack
+
+            let nextState = candidates |> Seq.filter isBlackNextIter |> Set.ofSeq
+            f nextState (iters-1)
+
+    f initState iters
+
+let testData = """sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew"""
+
+testData
+|> split "\n"
+|> Array.length
+
+let blackTilesTest =
+    testData
+    |> split "\n"
+    |> Array.map (parseTile >> tileCoord2)
+    |> Array.groupBy id
+    |> Array.filter (fun (_,arr) -> Array.length arr % 2 = 1)
+    |> Array.map fst
+
+blackTilesTest.Length
+
+solve2 blackTilesTest 100 |> Set.count
+
+let ans2 =
+    solve2 blackTiles 100
+    |> Set.count
 
 ans2
